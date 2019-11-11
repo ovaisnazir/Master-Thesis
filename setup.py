5ff3b7adb8f142c26309756f59704cf0b99cbec6
Víctor Chaparro Parra <victor.chaparro@gmail.com>
2019-11-11 08:34:57 +0100

    Trabajo Fin de Máster en Ingeniería de Sistemas de Decisión

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Trabajo final del máster en Ingenería de Sistemas de Decisión (https://www.urjc.es/estudios/master/915-ingenieria-de-sistemas-de-decision).',
+    author='Víctor Chaparro Parra',
+    license='',
+)
