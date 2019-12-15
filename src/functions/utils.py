e4c75478fd5c868d5e65600e6dad9ee785e6f7cd
Víctor Chaparro Parra <victor.chaparro@gmail.com>
2019-12-15 17:54:38 +0100

    function for teting  interpolate methods

--- src/functions/utils.py
+++ src/functions/utils.py
@@ -42,6 +42,57 @@ def reduce_mem_usage(df):
 
 def import_data(file):
     """create a dataframe and optimize its memory usage"""
-    df = pd.read_csv(file, parse_dates=True, keep_date_col=True)
+    df = pd.read_csv(file, keep_date_col=True)
     df = reduce_mem_usage(df)
     return df
+
+def make_ts_index(df, timeCol):
+    """create a datetime index from timeCol column"""
+    df_cpy = df.copy()
+    df_cpy[timeCol] = pd.to_datetime(df_cpy[timeCol])
+    df_cpy.index = df_cpy[timeCol]    
+    return df_cpy
+
+def test_interpolate_methods(df, col, methods, n, order):
+    '''
+      Function to test several methods of the interpolate() pandas method
+      for imputing NaN/NULL values. Inputs:
+          - df: data frame with no missing values in the column we want to test.
+          - col: string with the name of the variable.
+          - methods: list of interpolate methods to test.
+          - n: number of NaNs to be randomly imputed in the original data ts.
+          - order: order for spline/polynomial methods.
+    '''
+    df_cpy = df.copy()
+    
+    # Random selection of the time indexes 
+    nan_indexes = df_cpy.sample(n, replace=False, random_state=1).index
+    
+    # Impute nan's in a copy of the original df
+    df_cpy.loc[nan_indexes, col] = np.nan
+    
+    for m in methods:
+        if m in ['spline','polynomial']:
+            df_cpy[col].interpolate(method=m, order=order, inplace=True)         
+        else:
+            # Interpolation
+            df_cpy[col].interpolate(method=m, inplace=True)
+            df_cpy2 = df_cpy.copy()
+
+            # Error calculation
+            aprox = df_cpy.loc[list(nan_indexes), col]
+            real = df.loc[list(nan_indexes), col]
+            error_vec = (abs(aprox - real)/real) * 100
+            err_mean = np.mean(error_vec)
+            err_median = np.median(error_vec)
+
+            # Set back the NaN's for the next method
+            df_cpy.loc[nan_indexes, col] = np.nan
+            
+        print('Method {}:'.format(m))
+        print('Error vector:', error_vec)
+        print('Mean error: {0:.2f}'.format(err_mean))
+        print('Median error: {0:.2f}'.format(err_median))
+        print('===================================')
+    
+    return df_cpy2
